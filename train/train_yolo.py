4b5972093ed5baf7e594137f4af891d1f40afae5
Sara980710 <sara@idala.eu>
2022-03-01 17:54:37 +0100

    fixed directories in files

--- train/train_yolo.py
+++ train/train_yolo.py
@@ -6,9 +6,9 @@ train.run(
     imgsz=416,
     epochs=2,
     batch_size=64,
-    cfg="../models/yolov5n.yaml",
-    data="../datadef/airbus_kaggle.yaml",
-    weights="../yolov5n.pt",
-    project="../runs/train",
+    cfg="/models/yolov5n.yaml",
+    data="/datadef/airbus_kaggle.yaml",
+    weights="/yolov5n.pt",
+    project="/runs/train",
    # workers=2,
 )
