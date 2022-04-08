8c11a7f5b19ae79e9f5a81a23e50c6c4d40da405
Sara980710 <sara@idala.eu>
2022-04-08 13:11:14 +0200

    export

--- train/export_multiple.sh
+++ train/export_multiple.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 arr=( "32" "192" "384" "768" "1152" "1536" ) # Multiple of 32
+weights="/project/yolo_results/exp16/weights/best.pt"
+
 
 # Export float32
 for item in "${arr[@]}"
 do  
     echo " "
     echo "---------------------------------------------------"
-    echo "Running command: python3 yolov5/export.py --weights /project/yolo_results/exp16/weights/best.pt --include tflite --imgsz ${item} --name ${item} --keep_f32"
-    python3 yolov5/export.py --weights /project/yolo_results/exp16/weights/best.pt --include tflite --imgsz $item --name $item --keep_f32
+    echo "Running command: python3 yolov5/export.py --weights ${weights} --include tflite --imgsz ${item} --name ${item} --keep_f32"
+    python3 yolov5/export.py --weights $weights --include tflite --imgsz $item --name $item --keep_f32
 done
 
 # Export float16
@@ -16,15 +18,15 @@ for item in "${arr[@]}"
 do  
     echo " "
     echo "---------------------------------------------------"
-    echo "python3 yolov5/export.py --weights /project/yolo_results/exp16/weights/best.pt --include tflite --imgsz ${item} --name ${item}"
-    python3 yolov5/export.py --weights /project/yolo_results/exp16/weights/best.pt --include tflite --imgsz $item --name $item
+    echo "python3 yolov5/export.py --weights ${weights} --include tflite --imgsz ${item} --name ${item}"
+    python3 yolov5/export.py --weights $weights --include tflite --imgsz $item --name $item
 done
 
-# Export float32
+# Export int8
 for item in "${arr[@]}"
 do
     echo " "
     echo "---------------------------------------------------"
-    echo "python3 yolov5/export.py --weights /project/yolo_results/exp16/weights/best.pt --include tflite --imgsz ${item} --name ${item} --int8"
-    python3 yolov5/export.py --weights /project/yolo_results/exp16/weights/best.pt --include tflite --imgsz $item --name $item --int8
+    echo "python3 yolov5/export.py --weights ${weights} --include tflite --imgsz ${item} --name ${item} --int8 --data datadef/airbus_kaggle_aiqu.yaml"
+    python3 yolov5/export.py --weights $weights --include tflite --imgsz $item --name $item --int8 --data datadef/airbus_kaggle_aiqu.yaml
 done
